2606fae60dc790a172a0d7523928372079ead8c5
Minhkhoa2309 <tnmkhoa@gmail.com>
2022-04-06 23:26:49 +0700

    Fix Bug

--- NachOS-4.0/code/filesys/filesys.h
+++ NachOS-4.0/code/filesys/filesys.h
@@ -1,4 +1,4 @@
-// filesys.h 
+// filesys.h
 //	Data structures to represent the Nachos file system.
 //
 //	A file system is a set of files stored on disk, organized
@@ -8,26 +8,26 @@
 //	"open" file (read, write, close) are to be found in the OpenFile
 //	class (openfile.h).
 //
-//	We define two separate implementations of the file system. 
-//	The "STUB" version just re-defines the Nachos file system 
+//	We define two separate implementations of the file system.
+//	The "STUB" version just re-defines the Nachos file system
 //	operations as operations on the native UNIX file system on the machine
 //	running the Nachos simulation.
 //
-//	The other version is a "real" file system, built on top of 
-//	a disk simulator.  The disk is simulated using the native UNIX 
-//	file system (in a file named "DISK"). 
+//	The other version is a "real" file system, built on top of
+//	a disk simulator.  The disk is simulated using the native UNIX
+//	file system (in a file named "DISK").
 //
-//	In the "real" implementation, there are two key data structures used 
+//	In the "real" implementation, there are two key data structures used
 //	in the file system.  There is a single "root" directory, listing
 //	all of the files in the file system; unlike UNIX, the baseline
-//	system does not provide a hierarchical directory structure.  
+//	system does not provide a hierarchical directory structure.
 //	In addition, there is a bitmap for allocating
 //	disk sectors.  Both the root directory and the bitmap are themselves
 //	stored as files in the Nachos file system -- this causes an interesting
-//	bootstrap problem when the simulated disk is initialized. 
+//	bootstrap problem when the simulated disk is initialized.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #ifndef FS_H
@@ -38,32 +38,39 @@
 #include "openfile.h"
 #include "filetable.h"
 
-#ifdef FILESYS_STUB 		
+
+#ifdef FILESYS_STUB  // Temporarily implement file system calls as
+// calls to UNIX, until the real file system
+// implementation is available
 class FileSystem {
-  public:
-  	FileTable **fileTable;
+   public:
+    FileTable **fileTable;
+
     FileSystem() {
-		fileTable = new FileTable *[10];
+        fileTable = new FileTable *[10];
         for (int i = 0; i < 10; i++) {
             fileTable[i] = new FileTable;
         }
-	}
+    }
+
+    ~FileSystem() {
+        for (int i = 0; i < 10; i++) {
+            delete fileTable[i];
+        }
+        delete[] fileTable;
+    }
 
     bool Create(char *name) {
-		int fileDescriptor = OpenForWrite(name);
+        int fileID = OpenForWrite(name);
 
-		if (fileDescriptor == -1) return FALSE;
-		Close(fileDescriptor); 
-		return TRUE; 
-	}
+        if (fileID == -1) return FALSE;
+        Close(fileID);
+        return TRUE;
+    }
 
-    OpenFile* Open(char *name) {
-	  int fileDescriptor = OpenForReadWrite(name, FALSE);
+    OpenFile *Open(char *name);
 
-	  if (fileDescriptor == -1) return NULL;
-	  return new OpenFile(fileDescriptor);
-      }
-	int FileTableIndex();
+    int FileTableIndex();
 
     void Renew(int id) {
         for (int i = 0; i < 10; i++) {
@@ -75,9 +82,7 @@ class FileSystem {
         return fileTable[FileTableIndex()]->Insert(name, openMode);
     }
 
-    int Close(int id) {
-         return fileTable[FileTableIndex()]->Remove(id); 
-    }
+    int Close(int id) { return fileTable[FileTableIndex()]->Remove(id); }
 
     int Read(char *buffer, int charCount, int id) {
         return fileTable[FileTableIndex()]->Read(buffer, charCount, id);
@@ -90,89 +95,33 @@ class FileSystem {
     int Seek(int position, int id) {
         return fileTable[FileTableIndex()]->Seek(position, id);
     }
-    bool Remove(char *name) { return Unlink(name) == 0; }
 
+    bool Remove(char *name) { return Unlink(name) == 0; }
 };
 
-#else // FILESYS
+#else  // FILESYS
 class FileSystem {
-  public:
-  	FileTable **fileTable;
-    FileSystem() {
-		fileTable = new FileTable *[10];
-        for (int i = 0; i < 10; i++) {
-            fileTable[i] = new FileTable;
-        }
-	}
+   public:
+    FileSystem(bool format);  
 
-    bool Create(char *name) {
-		int fileDescriptor = OpenForWrite(name);
+    bool Create(char* name, int initialSize);
+    // Create a file (UNIX creat)
 
-		if (fileDescriptor == -1) return FALSE;
-		Close(fileDescriptor); 
-		return TRUE; 
-	}
+    OpenFile* Open(char* name);  // Open a file (UNIX open)
 
-    OpenFile* Open(char *name) {
-	  int fileDescriptor = OpenForReadWrite(name, FALSE);
+    bool Remove(char* name);  // Delete a file (UNIX unlink)
 
-	  if (fileDescriptor == -1) return NULL;
-	  return new OpenFile(fileDescriptor);
-      }
-	int FileTableIndex(){
-        return kernel->currentThread->processID; 
-    }
+    void List();  // List all the files in the file system
 
-    void Renew(int id) {
-        for (int i = 0; i < 10; i++) {
-            fileTable[id]->Remove(i);
-        }
-    }
+    void Print();  // List all the files and their contents
 
-    int Open(char *name, int openMode) {
-        return fileTable[FileTableIndex()]->Insert(name, openMode);
-    }
-
-    int Close(int id) {
-         return fileTable[FileTableIndex()]->Remove(id); 
-    }
-
-    int Read(char *buffer, int charCount, int id) {
-        return fileTable[FileTableIndex()]->Read(buffer, charCount, id);
-    }
-
-    int Write(char *buffer, int charCount, int id) {
-        return fileTable[FileTableIndex()]->Write(buffer, charCount, id);
-    }
-
-    int Seek(int position, int id) {
-        return fileTable[FileTableIndex()]->Seek(position, id);
-    }
-    bool Remove(char *name) { return Unlink(name) == 0; }
-    FileSystem(bool format);		// Initialize the file system.
-					// Must be called *after* "synchDisk" 
-					// has been initialized.
-    					// If "format", there is nothing on
-					// the disk, so initialize the directory
-    					// and the bitmap of free blocks.
-
-    bool Create(char *name, int initialSize);  	
-					// Create a file (UNIX creat)
-	// Open a file (UNIX open)
-
-    bool Remove(char *name);  		// Delete a file (UNIX unlink)
-
-    void List();			// List all the files in the file system
-
-    void Print();			// List all the files and their contents
-
-  private:
-   OpenFile* freeMapFile;		// Bit map of free disk blocks,
-					// represented as a file
-   OpenFile* directoryFile;		// "Root" directory -- list of 
-					// file names, represented as a file
+   private:
+    OpenFile* freeMapFile;    // Bit map of free disk blocks,
+                              // represented as a file
+    OpenFile* directoryFile;  // "Root" directory -- list of
+                              // file names, represented as a file
 };
 
-#endif // FILESYS
+#endif  // FILESYS
 
-#endif // FS_H
+#endif  // FS_H
